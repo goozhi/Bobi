68540e039877e0ca4a304aaef96a083002ce2208
637f075a0567bd2c57c77830 <637f075a0567bd2c57c77830@devide>
2023-06-21 09:52:26 +0000

    修改：     app.js 	修改：     package.json

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 
 const dirName = path.join(__dirname, 'assets');
@@ -22,12 +23,71 @@ app.use(async (ctx, next) => {
 });
 
 // response
-
+// index page
 app.use(async (ctx, next) => {
-  const html = fs.readFileSync(`${dirName}/index.html`);
-  ctx.res.setHeader('Content-Type','text/html;charset=utf-8');
-  ctx.body = html;
+  if (ctx.path === '/') {
+    const html = fs.readFileSync(`${dirName}/index.html`);
+    ctx.res.setHeader('Content-Type','text/html;charset=utf-8');
+    ctx.body = html;
+  } else {
+    await next();
+  }
 });
+
+// about page
+app.use(async (ctx, next) => {
+  if (ctx.path === '/about') {
+    if (ctx.method === 'GET') {
+      const html = `
+        <html>
+          <body>
+            <h1>About Page</h1>
+            <form method="POST">
+              <label for="name">Name:</label>
+              <input type="text" name="name" id="name" required>
+              <br>
+              <label for="email">Email:</label>
+              <input type="email" name="email" id="email" required>
+              <br>
+              <label for="message">Message:</label>
+              <textarea name="message" id="message" required></textarea>
+              <br>
+              <input type="submit" value="Submit">
+            </form>
+          </body>
+        </html>
+      `;
+      ctx.body = html;
+    } else if (ctx.method === 'POST') {
+      // ctx.request.body
+      const html2 = `
+        <html>
+          <body>
+            <h1>About Page</h1>
+            <form method="POST">
+              <label for="name">fuck name:</label>
+              <input type="text" name="name" id="name" required>
+              <br>
+              <label for="email">Email:</label>
+              <input type="email" name="email" id="email" required>
+              <br>
+              <label for="message">Message:</label>
+              <textarea name="message" id="message" required></textarea>
+              <br>
+              <input type="submit" value="Submit">
+            </form>
+          </body>
+        </html>
+      `;
+      ctx.body = html2;
+    }
+  } else {
+    await next();
+  }
+});
+
+// body parser
+app.use(bodyParser());
 
 app.listen(9000, () => {
   console.log(`app listening at http://localhost:9000`)
