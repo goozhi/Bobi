c2fd134eb31f4e4ce0f507aaf5b10b2bdac54ef5
wrvr <13120382+wrvr@user.noreply.gitee.com>
2023-07-25 00:44:30 +0000

    update app.js.
    Signed-off-by: wrvr <13120382+wrvr@user.noreply.gitee.com>

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const dirName = path.join(__dirname, 'assets');
 // logger
 
 app.use(async (ctx, next) => {
-  await next();
-  const rt = ctx.response.get('X-Response-Time');
-  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
 });
 // body parser
 app.use(bodyParser());
@@ -19,36 +19,36 @@ app.use(bodyParser());
 // x-response-time
 
 app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
 });
 
 // response
 // index page
 const obj_ybkc = {}
 const href_fictions = [...fictions].map(([key, value]) => {
-  const path_fiction = key.bookName.split('').map(ele => ele.charCodeAt(0)).join('-')
-  obj_ybkc[path_fiction] = key
-  return `<a href="/${path_fiction}" target="_blank" class="button--grey">${key.bookName}</a>`
+    const path_fiction = key.bookName.split('').map(ele => ele.charCodeAt(0)).join('-')
+    obj_ybkc[path_fiction] = key
+    return `<a href="/${path_fiction}" target="_blank" class="button--grey">${key.bookName}</a>`
 })
 
 app.use(async (ctx, next) => {
-  if (ctx.path === '/') {
-    const html = fs.readFileSync(`${dirName}/index.html`).toString().replace(/.*\/gusi.*/, href_fictions.join('\n'));
-    ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8');
-    ctx.body = html;
-  } else {
-    await next();
-  }
+    if (ctx.path === '/') {
+        const html = fs.readFileSync(`${dirName}/index.html`).toString().replace(/.*\/gusi.*/, href_fictions.join('\n'));
+        ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8');
+        ctx.body = html;
+    } else {
+        await next();
+    }
 });
 
 // about page
 app.use(async (ctx, next) => {
-  if (ctx.path === '/func') {
-    if (ctx.method === 'GET') {
-      const html = `
+    if (ctx.path === '/func') {
+        if (ctx.method === 'GET') {
+            const html = `
         <html>
           <body>
             <h1>About Page</h1>
@@ -61,15 +61,15 @@ app.use(async (ctx, next) => {
           </body>
         </html>
       `;
-      ctx.body = html;
-    } else if (ctx.method === 'POST') {
-      const message = ctx.request.body.message;
-      if (!message) {
-        throw new Error(`error`)
-      }
-      await commd(message).then(result => {
-        outputText = result.outputText
-        const html = `
+            ctx.body = html;
+        } else if (ctx.method === 'POST') {
+            const message = ctx.request.body.message;
+            if (!message) {
+                throw new Error(`error`)
+            }
+            await commd(message).then(result => {
+                outputText = result.outputText
+                const html = `
         <html>
         <body>
           <h1>About Page</h1>
@@ -95,35 +95,36 @@ app.use(async (ctx, next) => {
       </html>
 
     `;
-        ctx.body = html;
-      }).catch(err => ctx.body = err.stack || err)
-    }
+                ctx.body = html;
+            }).catch(err => ctx.body = err.stack || err)
+        }
 
-  } else if (ctx.path === '/about') {
-    const html = fs.readFileSync(`${dirName}/about.html`);
-    ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8');
-    ctx.body = html;
-  } else {
-    await next();
-  }
+    } else if (ctx.path === '/about') {
+        const html = fs.readFileSync(`${dirName}/about.html`);
+        ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8');
+        ctx.body = html;
+    } else {
+        await next();
+    }
 });
 
 app.use(async (ctx, next) => {
-  const gkjq_yj_ab = Object.entries(obj_ybkc).some(([key, value]) => {
-    if ('/' + key === ctx.path) {
-      const html = fs.readFileSync(`${dirName}/fiction.html`).toString().replace(/fiction-content/, [...fictions.get(value)].map(([key, value], index_1) => `第${index_1+1}章<h2>${value.title}</h2>` + value.content).join('\n\n')).replace(/\n/g, '<br>').replace(/fiction-title/, value.bookName);
-      ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8');
-      ctx.body = html;
-      return true
-    }
-  })
-  if (gkjq_yj_ab) {
+    const gkjq_yj_ab = Object.entries(obj_ybkc).some(([key, value]) => {
+        if ('/' + key === ctx.path) {
+            const html = fs.readFileSync(`${dirName}/fiction.html`).toString().replace(/fiction-content/, [...fictions.get(value)].map(([key, value], index_1) => `第${index_1 + 1}章<h2>${value.title}</h2>` + value.content).join('\n\n')).replace(/\n/g, '<br>').replace(/fiction-title/, value.bookName);
+            ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8');
+            ctx.body = html;
+            return true
+        }
+    })
+    if (gkjq_yj_ab) {
 
-  } else {
-    await next()
-  }
+    } else {
+        await next()
+    }
 })
 
-app.listen(80, () => {
-  console.log(`app listening at http://localhost:80`)
+const port = 9000
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
 });
