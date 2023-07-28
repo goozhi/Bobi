24635a9875cfb2a1d69d0c63344a45514a94e16e
wrvr <974054997@qq.com>
2023-07-28 15:54:00 +0800

    去你妈的

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const yxna_caju = ['wjdk-vktm', 'afoa', 'yxna-caju', 'hsoy-esqt']
 const neig = require('./neig')
 const uzms = require('./afoa/uz_ms')
 const send = require('koa-send')
+var vnwm_1
+var yxna_esqt
 // logger
 
 app.use(async (ctx, next) => {
@@ -187,28 +189,33 @@ app.use(async (ctx, next) => {
 app.use(async (ctx, next) => {
     if (ctx.path === '/dqab-esqt') {
         const yxna_hsoy_esqt = '/storage/emulated/0/\u7a00\u6709\u8f6f\u4ef6/'
-        if (!fs.existsSync(yxna_hsoy_esqt)) {
-            try {
-                uzms('csrf-yxna ac zznq-' + yxna_hsoy_esqt)
-            } catch (err) {
-                ctx.body = err.toString()
-                return
+        if(ctx.method==='POST'){
+            if (!fs.existsSync(yxna_hsoy_esqt)) {
+                try {
+                    uzms('csrf-yxna ac zznq-' + yxna_hsoy_esqt)
+                } catch (err) {
+                    ctx.body = err.toString()
+                    return
+                }
             }
-        }
-        const vnwm_2 = fs.readdirSync(yxna_hsoy_esqt, { recursive: true })
-        const vnwm_1 = vnwm_2.filter(rn1 => rn1.includes(ctx.request.body.esqt_wu))
-        if (vnwm_1.length) {
-            if (vnwm_1.length > 1) {
-                ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8')
-                ctx.body = 'rt lzca vdzv esqt_wu: ' + vnwm_1.join(' ae ')
+            const vnwm_2 = fs.readdirSync(yxna_hsoy_esqt, { recursive: true })
+            vnwm_1 = vnwm_2.filter(rn1 => rn1.includes(ctx.request.body.esqt_wu))
+            if (vnwm_1.length) {
+                if (vnwm_1.length > 1) {
+                    ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8')
+                    ctx.body = 'rt lzca vdzv esqt_wu: ' + vnwm_1.join(' ae ')
+                } else {
+                    yxna_esqt = path.join(yxna_hsoy_esqt, vnwm_1[0])
+                    ctx.attachment(yxna_esqt)
+                    await send(ctx, vnwm_1[0], { root: yxna_hsoy_esqt })
+                }
             } else {
-                const yxna_esqt = path.join(yxna_hsoy_esqt, vnwm_1[0])
-                ctx.attachment(yxna_esqt)
-                await send(ctx, 'AA逆向助手_50732.apk', { root: '/storage/emulated/0/稀有软件/' })
+                ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8')
+                ctx.body = 'hmpc diyc dk esqt.'
             }
-        } else {
-            ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8')
-            ctx.body = 'hmpc diyc dk esqt.'
+        }else{
+            ctx.attachment(yxna_esqt)
+            await send(ctx, vnwm_1[0], { root: yxna_hsoy_esqt })
         }
     } else {
         await next()
