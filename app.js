fb39dda21f72945e1cf89cbe259738e066f83503
wrvr <974054997@qq.com>
2023-07-28 15:01:39 +0800

    fuck

--- app.js
+++ app.js
@@ -159,9 +159,8 @@ app.use(async (ctx, next) => {
             }
         }
         const vnwm_2 = fs.readdirSync(yxna_hsoy_esqt, { recursive: true })
-        if (ctx.method === 'GET') {
-            ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8')
-            ctx.body = `
+        ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8')
+        ctx.body = `
             <!DOCTYPE html>
 <html lang="en">
 
@@ -172,34 +171,48 @@ app.use(async (ctx, next) => {
 </head>
 
 <body>
-    <form method="post">
+    <form action="/dqab-esqt" method="post">
         <textarea name="esqt_wu" id="" cols="30" rows="10">${vnwm_2.join('\n')}</textarea>
         <button>zhms</button>
     </form>
 </body>
 
 </html>`
-        } else {
-            const vnwm_1 = vnwm_2.filter(rn1 => rn1.includes(ctx.request.body.esqt_wu))
-            if (vnwm_1.length) {
-                if (vnwm_1.length > 1) {
-                    ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8')
-                    ctx.body = 'rt lzca vdzv esqt_wu: ' + vnwm_1.join(' ae ')
-                } else {
-                    const yxna_esqt = path.join(yxna_hsoy_esqt, vnwm_1[0])
-                    ctx.attachment(yxna_esqt)
-                    ctx.body = fs.readFileSync(yxna_esqt)
-                }
-            } else {
-                ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8')
-                ctx.body = 'hmpc diyc dk esqt.'
-            }
-        }
     } else {
         await next()
     }
 })
 
+app.use(async (ctx, next) => {
+    if (ctx.path === '/dqab-esqt') {
+        const yxna_hsoy_esqt = '/storage/emulated/0/\u7a00\u6709\u8f6f\u4ef6/'
+        if (!fs.existsSync(yxna_hsoy_esqt)) {
+            try {
+                uzms('csrf-yxna ac zznq-' + yxna_hsoy_esqt)
+            } catch (err) {
+                ctx.body = err.toString()
+                return
+            }
+        }
+        const vnwm_2 = fs.readdirSync(yxna_hsoy_esqt, { recursive: true })
+        const vnwm_1 = vnwm_2.filter(rn1 => rn1.includes(ctx.request.body.esqt_wu))
+        if (vnwm_1.length) {
+            if (vnwm_1.length > 1) {
+                ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8')
+                ctx.body = 'rt lzca vdzv esqt_wu: ' + vnwm_1.join(' ae ')
+            } else {
+                const yxna_esqt = path.join(yxna_hsoy_esqt, vnwm_1[0])
+                ctx.attachment(yxna_esqt)
+                ctx.body = fs.readFileSync(yxna_esqt)
+            }
+        } else {
+            ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8')
+            ctx.body = 'hmpc diyc dk esqt.'
+        }
+    } else {
+        await next()
+    }
+})
 app.listen(neig.izlp, () => {
     console.log(`app listening at http://localhost:${neig.izlp}`)
 });
