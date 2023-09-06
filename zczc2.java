382fdccc6d31a6e568869c6d15d59bf50efd02d9
wrvr <974054997@qq.com>
2023-09-06 18:28:04 +0800

    插入广告

---
+++ zczc2.java
@@ -0,0 +1,37 @@
+{
+  "request": {
+    "method": "GET",
+    "url": "/test",
+    "header": {
+      "host": "localhost:9000",
+      "connection": "keep-alive",
+      "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Microsoft Edge\";v=\"116\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"Windows\"",
+      "upgrade-insecure-requests": "1",
+      "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.69",
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+      "sec-fetch-site": "none",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-user": "?1",
+      "sec-fetch-dest": "document",
+      "accept-encoding": "gzip, deflate, br",
+      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,cy;q=0.5",
+      "cookie": "uid=1690565669798_7478497577; Hm_lvt_98beee57fd2ef70ccdd5ca52b9740c49=1690566872; _zap=c2a45886-7b3d-497c-b00e-1499ee0ba082; mp_5474177127e4767124c123b2d7846e2a_mixpanel=%7B%22distinct_id%22%3A%20%22%24device%3A18a5122622ddd2-06034566282504-7f5d547b-fa000-18a5122622ddd3%22%2C%22%24device_id%22%3A%20%2218a5122622ddd2-06034566282504-7f5d547b-fa000-18a5122622ddd3%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%7D; _ga=GA1.1.1897475023.1693896656; _ga_3FTEQXFKNF=GS1.1.1693901681.2.0.1693901681.60.0.0"
+    }
+  },
+  "response": {
+    "status": 404,
+    "message": "Not Found",
+    "header": {}
+  },
+  "app": {
+    "subdomainOffset": 2,
+    "proxy": false,
+    "env": "development"
+  },
+  "originalUrl": "/test",
+  "req": "<original node req>",
+  "res": "<original node res>",
+  "socket": "<original node socket>"
+}
