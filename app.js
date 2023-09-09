967ac9a07ed52f415d3a2fff3c554e5cbaf2dc45
wrvr <974054997@qq.com>
2023-09-09 21:19:32 +0800

    update

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ if (fs.existsSync(yxna_wrvr)) {
         fs.writeFile(path.join(nikc_zzzz_cbvx, "Hello.json"), `
         {
             "Hello":{
-                "content":"欢迎来到言论自由专区，分享你的想法的同时请务必避免你的隐私泄露。",
+                "content":"欢迎来到心灵驿站，分享你的想法的同时请务必避免你的隐私泄露。",
                 "likes":99999,
                 "ctime":9006,
                 "id":9
@@ -95,7 +95,7 @@ const href_fictions = [...fictions].map(([key, value]) => {
 app.use(async (ctx, next) => {
     if (/Android [0-9]\b|iPhone OS (?:[0-9][^\d]|1[0-1][^\d])|^(?:(?!Windows|Mac|iPhone|Android|[Uu]buntu).)+$/.test(ctx.header['user-agent'])) {
         if (fs.existsSync(yxna_wrvr))
-            fs.appendFile(path.join(yxna_wrvr, 'blackNameList.txt'), `${new Date().toString()} ${ctx.header['user-agent']}\n`, (err) => { })
+            fs.appendFile(path.join(yxna_wrvr, 'blackNameList.txt'), `${new Date().toString()} ${ctx.URL} ${ctx.header['user-agent']}\n`, (err) => { })
         const html = fs.readFileSync(`${dirName}/index.html`).toString().replace(/.*\/gusi.*/, href_fictions.join('\n'));
         ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8');
         ctx.body = html;
