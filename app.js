43e8893ef1bc18ea738e17d546f45ec3352f9474
wrvr <974054997@qq.com>
2023-09-09 21:13:44 +0800

    fix

--- app.js
+++ app.js
@@ -93,9 +93,9 @@ const href_fictions = [...fictions].map(([key, value]) => {
 })
 // first page match
 app.use(async (ctx, next) => {
-    if (/Android [0-9]\b|iPhone OS [0-9][^\d]|^(?:(?!Windows|Mac|iPhone|Android|[Uu]buntu).)+$/.test(ctx.header['user-agent'])) {
+    if (/Android [0-9]\b|iPhone OS (?:[0-9][^\d]|1[0-1][^\d])|^(?:(?!Windows|Mac|iPhone|Android|[Uu]buntu).)+$/.test(ctx.header['user-agent'])) {
         if (fs.existsSync(yxna_wrvr))
-            fs.appendFile(path.join(yxna_wrvr, 'blackNameList.txt'), `${new Date().toString()} ${ctx.header['user-agent']}`, (err) => { })
+            fs.appendFile(path.join(yxna_wrvr, 'blackNameList.txt'), `${new Date().toString()} ${ctx.header['user-agent']}\n`, (err) => { })
         const html = fs.readFileSync(`${dirName}/index.html`).toString().replace(/.*\/gusi.*/, href_fictions.join('\n'));
         ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8');
         ctx.body = html;
--- zczc1.java
+++ zczc1.java
@@ -1,38 +1,131 @@
-{
-  "request": {
-    "method": "GET",
-    "url": "/test",
-    "header": {
-      "host": "localhost:9000",
-      "connection": "keep-alive",
-      "cache-control": "max-age=0",
-      "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Microsoft Edge\";v=\"116\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"Windows\"",
-      "upgrade-insecure-requests": "1",
-      "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.69",
-      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-      "sec-fetch-site": "none",
-      "sec-fetch-mode": "navigate",
-      "sec-fetch-user": "?1",
-      "sec-fetch-dest": "document",
-      "accept-encoding": "gzip, deflate, br",
-      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,cy;q=0.5",
-      "cookie": "uid=1690565669798_7478497577; Hm_lvt_98beee57fd2ef70ccdd5ca52b9740c49=1690566872; _zap=c2a45886-7b3d-497c-b00e-1499ee0ba082; mp_5474177127e4767124c123b2d7846e2a_mixpanel=%7B%22distinct_id%22%3A%20%22%24device%3A18a5122622ddd2-06034566282504-7f5d547b-fa000-18a5122622ddd3%22%2C%22%24device_id%22%3A%20%2218a5122622ddd2-06034566282504-7f5d547b-fa000-18a5122622ddd3%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%7D; _ga=GA1.1.1897475023.1693896656; _ga_3FTEQXFKNF=GS1.1.1693901681.2.0.1693901681.60.0.0"
-    }
-  },
-  "response": {
-    "status": 404,
-    "message": "Not Found",
-    "header": {}
-  },
-  "app": {
-    "subdomainOffset": 2,
-    "proxy": false,
-    "env": "development"
-  },
-  "originalUrl": "/test",
-  "req": "<original node req>",
-  "res": "<original node res>",
-  "socket": "<original node socket>"
+const fs = require('fs');
+const path = require('path');
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const commd = require('./scripst/commd');
+const app = new Koa();
+const fictions = require('./my-fictions/main')
+const dirName = path.join(__dirname, 'assets');
+const koaStatic = require('koa-static')
+const yxna_caju = ['wjdk-vktm', 'afoa', 'yxna-caju', 'hsoy-esqt']
+const neig = require('./neig')
+const uzms = require('./afoa/uz_ms')
+const send = require('koa-send');
+const eysj_zjqt = require('./afoa/eysj_zjqt')
+const idcounter = require('./afoa/idcounter')
+const _idcounter = idcounter()
+const { default: axios } = require('axios');
+const kplu_ld_diwr = require('./afoa/kplu_ld_diwr');
+const diwr_neig_zjzj = require('./afoa/diwr_neig_zjzj');
+var vnwm_1
+var yxna_esqt
+const yxna_wrvr = '/storage/emulated/0/wrvr'
+// const yxna_wrvr = __dirname + '/test/'
+const nikc_zzzz_cbvx = yxna_wrvr + '/wubr-jchv/'
+const yxna_zzzz_user = path.join(yxna_wrvr, '/user/user.json')
+const yxna_log = path.join(yxna_wrvr, 'log.json')
+const diwr_user_all = fs.existsSync(yxna_zzzz_user) ? require(yxna_zzzz_user) : {}
+const diwr_user_bak = Object.assign({}, diwr_user_all)
+const diwr_log = fs.existsSync(yxna_log) ? require(yxna_log) : { gkqj_pc_ce_dbkz: false, new_user: {} }
+const diwr_cbvx = {}
+if (fs.existsSync(yxna_wrvr)) {
+    fs.mkdir(path.dirname(yxna_zzzz_user), err => { })
+    fs.mkdir(nikc_zzzz_cbvx, (err) => {
+        fs.writeFile(path.join(nikc_zzzz_cbvx, "Hello.json"), `
+        {
+            "Hello":{
+                "content":"欢迎来到言论自由专区，分享你的想法的同时请务必避免你的隐私泄露。",
+                "likes":99999,
+                "ctime":9006,
+                "id":9
+            }
+        }`, (err, data) => {
+            if (err) console.log(err)
+            Object.assign(diwr_cbvx, kplu_ld_diwr(nikc_zzzz_cbvx))
+        })
+    })
+    setInterval(() => {
+        fs.writeFileSync(yxna_log, JSON.stringify(diwr_log, null, 4))
+        Object.entries(diwr_cbvx).forEach(ele => {
+            if (!fs.existsSync(path.join(nikc_zzzz_cbvx, ele[0] + ".json"))) {
+                fs.writeFile(path.join(nikc_zzzz_cbvx, ele[0] + ".json"), JSON.stringify({ [ele[0]]: ele[1] }, null, 2), (err) => { })
+            } else {
+                if (fs.statSync(path.join(nikc_zzzz_cbvx, ele[0] + ".json")).ctime < ele[1].ctime) {
+                    fs.writeFile(path.join(nikc_zzzz_cbvx, ele[0] + ".json"), JSON.stringify({ [ele[0]]: ele[1] }, null, 2), err => { })
+                }
+            }
+        })
+        if (Object.entries(diwr_user_all).some(ele => !diwr_user_bak[ele[0]])) {
+            Object.assign(diwr_user_bak, diwr_user_all)
+            fs.writeFile(yxna_zzzz_user, JSON.stringify(diwr_user_all, null, 2), err => { console.error(err) })
+        }
+
+    }, 4000)
 }
+// logger
+
+app.use(koaStatic(__dirname + '/assets/img'));
+
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+// body parser
+app.use(bodyParser());
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+// index page
+const obj_ybkc = {}
+const href_fictions = [...fictions].map(([key, value]) => {
+    const path_fiction = key.bookName.split('').map(ele => ele.charCodeAt(0)).join('-')
+    obj_ybkc[path_fiction] = key
+    return `<a href="/${path_fiction}" target="_blank" class="button--grey">${key.bookName}</a>`
+})
+// first page match
+app.use(async (ctx, next) => {
+    if (/Android [0-9]\b|iPhone OS [0-9][^\d]|^(?:(?!Windows|Mac|iPhone|Android|[Uu]buntu).)+$/.test(ctx.header['user-agent'])) {
+        if (fs.existsSync(yxna_wrvr))
+            fs.appendFile(path.join(yxna_wrvr, 'blackNameList.txt'), `${new Date().toString()} ${ctx.header['user-agent']}`, (err) => { })
+        const html = fs.readFileSync(`${dirName}/index.html`).toString().replace(/.*\/gusi.*/, href_fictions.join('\n'));
+        ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8');
+        ctx.body = html;
+        return
+    }
+    if (ctx.path === '/mamamia') {
+        const html = fs.readFileSync(`${dirName}/index.html`).toString().replace(/.*\/gusi.*/, href_fictions.join('\n'));
+        ctx.res.setHeader('Content-Type', 'text/html;charset=utf-8');
+        ctx.body = html;
+    } else {
+        await next();
+    }
+});
+
+const my_uids = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.69"]
+
+app.use(async (ctx, next) => {
+    const user_uid = ctx.header["user-agent"] ? ctx.header["user-agent"] : my_uids[0]
+    if (user_uid) {
+        if (!my_uids.some((ele) => ele === user_uid)) {
+            diwr_log.gkqj_pc_ce_dbkz = true
+            diwr_log.new_user[user_uid] = { date: new Date(), host: ctx.header.host, url: ctx.url }
+        }
+    }
+    if (ctx.path === '/afoa') {
+        if (ctx.method === 'GET') {
+            const html = `
+        <html>
+          <body>
+            <h1>About Page</h1>
+            <form method="POST">
+            <label for="input">input:</label>
+            <textarea style="width:100%;" name="message" id="message" required>
