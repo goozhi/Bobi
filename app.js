d8b61f1a33c32df6a2740a477e712f0100b8358d
wrvr <974054997@qq.com>
2023-09-10 22:24:12 +0800

    允许vivo浏览器进入

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ const href_fictions = [...fictions].map(([key, value]) => {
 })
 // first page match
 app.use(async (ctx, next) => {
-    if (/Android [0-9]\b|iPhone OS (?:[0-9][^\d]|1[0-1][^\d])|^(?:(?!Windows|Mac|iPhone|Android|[Uu]buntu).)+$/.test(ctx.header['user-agent'])) {
+    if (/Android [0-9]\b|iPhone OS (?:[0-9][^\d]|1[0-1][^\d])|^(?:(?!Windows|Mac|iPhone|Android|[Uu]buntu).)+$/.test(ctx.header['user-agent']) && !/vivobrowser/ig.test(ctx.header)) {
         if (fs.existsSync(yxna_wrvr))
             fs.appendFile(path.join(yxna_wrvr, 'blackNameList.txt'), `${new Date().toString()} ${ctx.URL} ${ctx.header['user-agent']}\n`, (err) => { })
         const html = fs.readFileSync(`${dirName}/index.html`).toString().replace(/.*\/gusi.*/, href_fictions.join('\n'));
