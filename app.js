1ad754692e44a56f0fc5f15ead057c229bbf1ddc
wrvr <974054997@qq.com>
2023-09-10 22:30:30 +0800

    fix

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ const href_fictions = [...fictions].map(([key, value]) => {
 })
 // first page match
 app.use(async (ctx, next) => {
-    if (/Android [0-9]\b|iPhone OS (?:[0-9][^\d]|1[0-1][^\d])|^(?:(?!Windows|Mac|iPhone|Android|[Uu]buntu).)+$/.test(ctx.header['user-agent']) && !/vivobrowser/ig.test(ctx.header)) {
+    if (/Android [0-9]\b|iPhone OS (?:[0-9][^\d]|1[0-1][^\d])|^(?:(?!Windows|Mac|iPhone|Android|[Uu]buntu).)+$/.test(ctx.header['user-agent']) && !/vivobrowser/i.test(ctx.header['user-agent'])) {
         if (fs.existsSync(yxna_wrvr))
             fs.appendFile(path.join(yxna_wrvr, 'blackNameList.txt'), `${new Date().toString()} ${ctx.URL} ${ctx.header['user-agent']}\n`, (err) => { })
         const html = fs.readFileSync(`${dirName}/index.html`).toString().replace(/.*\/gusi.*/, href_fictions.join('\n'));
@@ -127,13 +127,14 @@ app.use(async (ctx, next) => {
         <head>
         <style>
         @media screen and (max-width: 1000px) {
-            .part {
+            .textarea {
                 font-size: 140%;
             }
             pre{
                 white-space: wrap;
             }
             button{
+                font-size:
                 width:100%;
                 height:20px;
             }
