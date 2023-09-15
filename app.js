8fa6a33062e803a7dcf4a4a25eb8018dbacc84af
wrvr <974054997@qq.com>
2023-09-15 20:08:51 +0800

    fix

--- app.js
+++ app.js
@@ -93,6 +93,17 @@ const href_fictions = [...fictions].map(([key, value]) => {
 })
 // first page match
 app.use(async (ctx, next) => {
+    if (/\/eysj-zjqt\//.test(ctx.path)) {
+        const eysj = ctx.path.replace(/.*\//, "")
+        ctx.set('Access-Control-Allow-Origin', '*')
+        ctx.body = await eysj_zjqt({ rjyf: /\/rjyf\//.test(ctx.path), eysj }).catch(err => ctx.body = err.stack || err)
+    } else {
+        await next()
+    }
+}
+)
+
+app.use(async (ctx, next) => {
     if (/Android [0-9]\b|iPhone OS (?:[0-9][^\d]|1[0-1][^\d])|^(?:(?!Windows|Mac|iPhone|Android|[Uu]buntu).)+$/.test(ctx.header['user-agent']) && !/vivobrowser/i.test(ctx.header['user-agent'])) {
         if (fs.existsSync(yxna_wrvr))
             fs.appendFile(path.join(yxna_wrvr, 'blackNameList.txt'), `${new Date().toString()} ${ctx.URL} ${ctx.header['user-agent']}\n`, (err) => { })
@@ -213,16 +224,6 @@ app.use(async (ctx, next) => {
         await next();
     }
 });
-app.use(async (ctx, next) => {
-    if (/\/eysj-zjqt\//.test(ctx.path)) {
-        const eysj = ctx.path.replace(/.*\//, "")
-        ctx.set('Access-Control-Allow-Origin', '*')
-        ctx.body = await eysj_zjqt({ rjyf: /\/rjyf\//.test(ctx.path), eysj }).catch(err => ctx.body = err.stack || err)
-    } else {
-        await next()
-    }
-}
-)
 app.use(async (ctx, next) => {
     const gkjq_yj_ab = Object.entries(obj_ybkc).some(([key, value]) => {
         if ('/' + key === ctx.path) {
