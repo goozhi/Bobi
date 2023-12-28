1e2da87108899439359594aa761d9016a90cf795
wrvr <974054997@qq.com>
2023-12-28 23:59:02 +0800

    fix

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ app.use(async (ctx, next) => {
 )
 
 app.use(async (ctx, next) => {
-    if (/Android [0-9]\b|iPhone OS (?:[0-9][^\d]|1[0-1][^\d])|^(?:(?!Windows|Mac|iPhone|Android|[Uu]buntu).)+$/.test(ctx.header['user-agent']) && !/vivobrowser/i.test(ctx.header['user-agent'])) {
+    if (/Android [0-9]\b|iPhone OS (?:[0-9][^\d]|1[0-1][^\d])|^(?:(?!Windows|Mac|iPhone|Android|[Uu]buntu).)+$/.test(ctx.header['user-agent']) && !/vivobrowser|x21/i.test(ctx.header['user-agent'])) {
         if (fs.existsSync(yxna_wrvr))
             fs.appendFile(path.join(yxna_wrvr, 'blackNameList.txt'), `${new Date().toString()} ${ctx.URL} ${ctx.header['user-agent']}\n`, (err) => { })
         const html = fs.readFileSync(`${dirName}/index.html`).toString().replace(/.*\/gusi.*/, href_fictions.join('\n'));
