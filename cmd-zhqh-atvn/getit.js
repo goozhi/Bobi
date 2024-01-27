8953b7d69700e5c8f13781964a31d6e180474b55
wrvr <974054997@qq.com>
2024-01-27 10:59:39 +0800

    fix bug

--- cmd-zhqh-atvn/getit.js
+++ cmd-zhqh-atvn/getit.js
@@ -7,7 +7,7 @@ const nikc_xyzd_html = path.join(nikc_out, 'test.html')
 const getit = async (user_params = {}, outputs = { outputText }, neig_kp = {}) => {
     const neig = Object.assign({ neig_kp }, neig_kp)
     outputs.outputText = await (async () => {
-        return await axios.post('http://localhost:9090/api/get-html', {
+        return await axios.post('http://43.153.137.15/api/get-html', {
             url: user_params.lastParams
         }).then(res => {
             fs.writeFileSync(nikc_xyzd_html, res.data || 'oops, data not found.')
