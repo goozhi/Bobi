40e71b4603ca0ed32bc14acd3854b876264f6136
wrvr <974054997@qq.com>
2024-05-08 06:50:28 +0800

    add arrC of agle

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Koa = require('koa');
 const wdbu_err = require('../scripts/wdbu_err.js')
 const arrC = require("./arrC.js")
 const arrC_office = require('../office/arrC.js')
+const arrC_agle = require('../wjdk-agle/arrC.js')
 const arrC_en = require('../dicts-en/arrC.js')
 const ji_exym_oc_ssvl = fs.existsSync('/storage/emulated/0/')
 const { koaBody } = require('koa-body');
@@ -267,7 +268,7 @@ app.use(async (ctx, next) => {
             const html = fs.readFileSync(`${dirName}/afoa.html`).toString()
             ctx.body = html;
         } else if (ctx.method === 'POST') {
-            neig.excmds = [...arrC, ...arrC_en, ...arrC_office]
+            neig.excmds = [...arrC, ...arrC_en, ...arrC_office, ...arrC_agle]
             await commd(ctx.request.body.vdzv, outputs(outputs_office()), neig).then(jtyj_1 => {
                 ctx.body = jtyj_1
             })
