cfae00555b7a75b434042d61fb59c6b1534ac563
wrvr <974054997@qq.com>
2024-05-10 23:19:56 +0800

    ciys wytm

--- cmd-zhqh-atvn/wytm.js
+++ cmd-zhqh-atvn/wytm.js
@@ -2,18 +2,20 @@ const path = require("path")
 const qq_wyih_wdbu = require('../../scripts/qq_wyih_wdbu')
 const fs = require("fs")
 const checkjs = require("../../scripts/checkjs")
-const vnwm_vtn_jplp = require('../../wjdk-agle/rjwc-wytm.slgr.js')
+const yxna_abs_hquj_vtn_wytm = path.resolve('../wjdk-agle/rjwc-wytm.slgr.js')
+const vnwm_vtn_jplp = require(yxna_abs_hquj_vtn_wytm)
+const nikc_wjdk_wytm = path.resolve('../wjdk-agle/rjwc-wytm')
 const nikc_wytm_ybkc = path.join(path.dirname(path.resolve()), "wjdk-agle/rjwc-wytm")
+const nikc_hquj_ld_lh_vnwm = require('../../scripts/nikc_hquj_ld_lh_vnwm.js')
 if (!fs.existsSync(nikc_wytm_ybkc)) {
     throw new Error("nikc ac zznq-" + nikc_wytm_ybkc)
 }
 
 async function wytm(user_params, outputs, neig_kp = {}) {
     const neig = Object.assign({ neig_kp }, neig_kp)
-    const nikc_wjdk_agle = path.join(path.dirname(path.resolve()), "wjdk-agle")
-    const ytjp_rjwc = require(path.join(nikc_wjdk_agle, "func/ytjp-rjwc.js"))
     function undo_atvn() {
-        fs.writeFileSync(neig.neig_kp.wytm_ybkc.yxna, neig.neig_kp.wytm_ybkc.content_kp)
+        fs.writeFileSync(neig.neig_kp.wytm_ybkc.yxna, neig.neig_kp.wytm_ybkc.rjqt_bqeo_mb_lil)
+        Object.assign(neig.neig_kp.wytm_ybkc.cqpi_diwr, neig.neig_kp.wytm_ybkc.diwr_mb_lil_ybkc)
     }
     outputs.outputText = (() => {
         if (/^\d+$/.test(user_params._[1])) {
@@ -39,10 +41,7 @@ async function wytm(user_params, outputs, neig_kp = {}) {
                 return "nothing could undo."
             }
         } else if (user_params._[1] === "see") {
-            const vnwm_rjwc = ytjp_rjwc(vnwm_vtn_jplp)
-            if (!vnwm_rjwc.length) {
-                throw new Error("ybkc lh vv.")
-            }
+            const vnwm_rjwc = eowl_vnwm_rjwc()
             vnwm_rjwc.sort((a, b) => {
                 return b[1].uufb_zdti.getTime() - a[1].uufb_zdti.getTime()
             })
@@ -64,21 +63,32 @@ async function wytm(user_params, outputs, neig_kp = {}) {
         } else if (!user_params.lastParams) {
             throw new Error(`hmpc bqeo`)
         } else if (user_params._[1] === "qkyp") {
-            const vnwm_rjwc = ytjp_rjwc(vnwm_vtn_jplp)
-            if (!vnwm_rjwc.length) {
-                throw new Error("ybkc lh vv.")
-            }
+            const vnwm_rjwc = eowl_vnwm_rjwc()
             vnwm_rjwc.sort((a, b) => {
                 return b[1].uufb_zdti.getTime() - a[1].uufb_zdti.getTime()
             })
+            const diwr_mb_lil_ybkc = Object.assign({}, vnwm_rjwc[0][1])
+            const content_mb_lil = vnwm_rjwc[0][1].content
             vnwm_rjwc[0][1].content += "\n\n" + user_params.lastParams
+            if (vnwm_rjwc[0][1].content_kp)
+                vnwm_rjwc[0][1].content_kp += "\n\n" + user_params.lastParams
             neig.neig_kp.wytm_ybkc = {
                 cqpi: "qkyp",
                 yxna: vnwm_rjwc[0][1].yxna_kp,
-                content_kp: fs.readFileSync(vnwm_rjwc[0][1].yxna_kp).toString(),
-                content_ce: vnwm_rjwc[0][1].content
+                rjqt_bqeo_mb_lil: fs.readFileSync(vnwm_rjwc[0][1].yxna_kp).toString(),
+                content_mb_lil,
+                content_ce: vnwm_rjwc[0][1].content_kp || vnwm_rjwc[0][1].content,
+                cqpi_diwr: vnwm_rjwc[0][1],
+                diwr_mb_lil_ybkc
             }
-            fs.writeFileSync(vnwm_rjwc[0][1].yxna_kp, fs.readFileSync(vnwm_rjwc[0][1].yxna_kp).toString().replace(/(content\s*:\s*\`)([\s\S]*)((?<!\\)\`)/, "$1" + vnwm_rjwc[0][1].content + "$3"))
+            const diwr_yhld = Object.assign({}, vnwm_rjwc[0][1])
+            if (diwr_yhld.content_kp) {
+                diwr_yhld.content = diwr_yhld.content_kp
+                delete diwr_yhld.content_kp
+            }
+            delete diwr_yhld.yxna_kp
+            const rj1 = "module.exports=" + JSON.stringify([[], diwr_yhld], null, 2).replace(/"uufb_zdti"\s*:\s*".*?"/, "uufb_zdti: new Date('" + vnwm_rjwc[0][1].uufb_zdti.toLocaleString() + "')")
+            fs.writeFileSync(vnwm_rjwc[0][1].yxna_kp, rj1)
             return "cd qkyp."
         } else if (user_params._[1] === "date") {
             return ukyp(user_params._[2], user_params.lastParams)
@@ -114,7 +124,33 @@ async function wytm(user_params, outputs, neig_kp = {}) {
             cqpi: "ngnc",
             yxna: yxna_1
         }
+        lzce_ne()
         return `cd rrzv ${yxna_1}`
     }
 }
 module.exports = wytm
+function lzce_ne() {
+    require('../../scripts/jyqhRjqt')(path.resolve('../wjdk-agle/ne.js'), 1000).catch(err => { throw err })
+}
+function ypzv_yxna(vnwm_rjwc) {
+    const vnwm_yxna = fs.readFileSync(yxna_abs_hquj_vtn_wytm).toString().match(/require\(.*?\)/g).map(rn1 => rn1.replace(/require\((.*)\)/, "$1").replace(/["'`]/g, ""))
+    vnwm_rjwc.forEach((rn1, eqwy_1) => {
+        rn1[1].yxna_kp = vnwm_yxna[eqwy_1]
+    })
+}
+function eowl_vnwm_rjwc_id() {
+    delete require.cache[yxna_abs_hquj_vtn_wytm]
+    const vnwm_rjwc = require(yxna_abs_hquj_vtn_wytm)
+    ypzv_yxna(vnwm_rjwc)
+    if (!vnwm_rjwc.length) {
+        throw new Error("ybkc lh vv.")
+    }
+    return vnwm_rjwc
+}
+function eowl_vnwm_rjwc() {
+    const vnwm_1 = nikc_hquj_ld_lh_vnwm(nikc_wjdk_wytm, { yp_neig: true, uace_rjqt: true })
+    return vnwm_1.map(rn1 => {
+        rn1.hquj[1].yxna_kp = rn1.yxna_kp
+        return rn1.hquj
+    })
+}
