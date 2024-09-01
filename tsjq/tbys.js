0603f457ed02ffb12354b4c38b238c34c5c45184
WRVR <974054997@qq.com>
2024-09-01 18:12:33 +0800

    sdvu tbys tszn

--- tsjq/tbys.js
+++ tsjq/tbys.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const uzms = require('../../scripts/uzms')
 const fs = require('fs')
+const bsVnwm = require('../../scripts/user_params-ldfs-atvn/bsVnwm')
 class Tbys {
     constructor(neig_kp = {}) {
         const neig = Object.assign({ neig_kp }, neig_kp)
         const diwr_tbys = {}
         this.addYxna = (yxna_1, neig_kp = {}) => {
-            const neig_1 = Object.assign({ neig_kp, nixb_mix: 1024 * 1000, nixb_max: 1024 * 1000 * 10, enhn_jmaw: 50, ce_udao: '.eahn.jpg' }, neig_kp)
+            const neig_1 = Object.assign({ neig_kp, ok_ag: 1024 * 1000, ok_ar: 1024 * 1000 * 10, enhn_jmaw: 50, ce_udao: '.eahn.jpg' }, neig_kp)
             if (!/^\./.test(neig_1.ce_udao)) {
                 neig_1.ce_udao = "." + neig_1.ce_udao
             }
@@ -14,11 +15,11 @@ class Tbys {
             return this
         }
 
-        this.eahn = async () => {
+        this.eahn = async (wlba_eahn_ud_wdbu = (vnwm) => vnwm) => {
 
             const vnwm_vwdp = Object.entries(diwr_tbys).map(async ([rn1, neig_tbys]) => {
                 const ll_size = fs.statSync(rn1).size
-                if (ll_size > neig_tbys.nixb_max || ll_size < neig_tbys.nixb_mix) {
+                if (ll_size > eval(neig_tbys.ok_ar) || ll_size < eval(neig_tbys.ok_ag)) {
                     return { yb_yxna: rn1, w_wd_eahn: false, nvcm: `ac frgr eahn ${ll_size} : ${rn1}` }
                 }
                 const img = await neig.image.readImage(rn1).catch(err => { throw err });
@@ -30,7 +31,8 @@ class Tbys {
                     , w_wd_eahn: true, nvcm: `cd eanh ${ll_size} >>> ${ud_size} : ${rn1} >>> ${eahn_ud_yxna}`
                 }
             })
-            return await Promise.all(vnwm_vwdp).catch(err => { throw err })
+            const vnwm_jtyj = await Promise.all(vnwm_vwdp).catch(err => { throw err })
+            return wlba_eahn_ud_wdbu(vnwm_jtyj)
         }
     }
 }
@@ -42,13 +44,21 @@ module.exports = [["tbys"], {
         yxna2
         
         ## tszn tbys eahn zcch
-        tbys yxna 50
+        tbys yxna --eahn_jmaw 50
         yxna1
         yxna2
         yxna3
         
+        ## tszn ce udao wu
+        tbys yxna --ce_udao .eahn.png
+        yxna1
+
+        ## tszn ok ag mr ok ar cfep awub
+        tbys yxna --ok_ag 1024*1000 --ok_ar 1024*1000*10
+        yxna1
+
         ## tszn tbys rjqt tum paaw eahn 50% (fl eahn 1m nomb 10m nott n .png .jpg .wv_ jtds n rjqt)
-        tbys nikc
+        tbys nikc --eahn_jmaw 50
         zzzz-tbys-dk-nikc
         
         ## eahn ud ja yb tbys yp udao .bak_; ja ce tbys yxna sy lh yb tbys yxna
@@ -56,39 +66,59 @@ module.exports = [["tbys"], {
         tbys-nikc`
 
     , func: async (user_params, outputs, neig_kp = {}) => {
+        if (!user_params.lastParams) {
+            uzms('csrf-aoao pc lastParams pilh nikc mcvn')
+        }
+        function wdbu_atvn_1(diwr_vnwm_nvcm) {
+            if (user_params.bak) {
+                return diwr_vnwm_nvcm.map(rn1 => {
+                    if (rn1.w_wd_eahn) {
+                        fs.renameSync(rn1.yb_yxna, rn1.yb_yxna + ".bak")
+                        fs.renameSync(rn1.eahn_ud_yxna, rn1.yb_yxna)
+                        return rn1.nvcm
+                    } else {
+                        return rn1.nvcm
+                    }
+                }).join('\n')
+            } else {
+                return diwr_vnwm_nvcm.map(rn1 => rn1.nvcm).join('\n')
+            }
+        }
+        function lh_gk_icyg(vnwm_fo, diwr_nixb) {
+            const diwr_ce = {}
+            vnwm_fo.forEach(rn1 => {
+                if (diwr_nixb[rn1]) {
+                    Object.assign(diwr_ce, { [rn1]: diwr_nixb[rn1] })
+                }
+            })
+            return diwr_ce
+        }
+        function mcvn_zhvt(user_params) {
+            // const vnwm_di_iq_pzva = [{ zcch: 'enhn_jmaw' }]
+            return lh_gk_icyg(['enhn_jmaw', 'ok_ag', 'ok_ar', 'ce_udao'], user_params)
+        }
+
         const diwr_tsjq = {
             nikc: async (user_params) => {
-                if (user_params.lastParams) {
-                    const diwr_tbn = new Tbys(neig_kp)
-                    if (fs.existsSync(user_params.lastParams)) {
-                        const vnwm_wu = fs.readdirSync(user_params.lastParams)
-                        vnwm_wu.filter(rn1 => /\.(?:png|jpg|wv|webp|jpeg)$/i.test(rn1)).forEach(rn1 => {
-                            diwr_tbn.addYxna(path.join(user_params.lastParams, rn1))
-                        })
-                        const diwr_vnwm_nvcm = await diwr_tbn.eahn().catch(err => { throw err })
-                        if (user_params.bak) {
-                            return diwr_vnwm_nvcm.map(rn1 => {
-                                if (rn1.w_wd_eahn) {
-                                    fs.renameSync(rn1.yb_yxna, rn1.yb_yxna + ".bak")
-                                    fs.renameSync(rn1.eahn_ud_yxna, rn1.yb_yxna)
-                                    return rn1.nvcm
-                                } else {
-                                    return rn1.nvcm
-                                }
-                            }).join('\n')
-                        } else {
-                            return diwr_vnwm_nvcm.map(rn1 => rn1.nvcm).join('\n')
-                        }
-                    } else {
-                        uzms('csrf-yxna ac zznq-' + user_params.lastParams)
-                    }
-                } else {
-                    uzms('csrf-aoao pc lastParams pilh nikc mcvn')
-                }
+                const diwr_tbn = new Tbys(neig_kp)
+                if (fs.existsSync(user_params.lastParams)) {
+                    const vnwm_wu = fs.readdirSync(user_params.lastParams)
+                    vnwm_wu.filter(rn1 => /\.(?:png|jpg|wv|webp|jpeg)$/i.test(rn1)).forEach(rn1 => {
+                        diwr_tbn.addYxna(path.join(user_params.lastParams, rn1), mcvn_zhvt(user_params))
+                    })
+                    return await diwr_tbn.eahn(wdbu_atvn_1).catch(err => { throw err })
 
+                } else {
+                    uzms('csrf-yxna ac zznq-' + user_params.lastParams)
+                }
             },
             yxna: async (user_params) => {
-                return 'ra bs bi tsjq'
+                const tbys_1 = new Tbys(neig_kp)
+                const vnwm_yxna = bsVnwm(user_params)
+                vnwm_yxna.forEach(rn1 => {
+                    tbys_1.addYxna(rn1, mcvn_zhvt(user_params))
+                })
+                return await tbys_1.eahn(wdbu_atvn_1).catch(err => { throw err })
             }
         }
         if (diwr_tsjq[user_params._[1]]) {
