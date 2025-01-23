94e5fbf2314bb07c012a50311f3959be15f14971
wrvr <974054997@qq.com>
2025-01-23 13:41:26 +0800

    bv sql.js module

--- test.js
+++ test.js
@@ -1,4 +1,14 @@
-const fs = require('fs')
-const data = fs.readFileSync('D:/RSGM/nodejs/Koa/logo.ico')
-console.log(fs.writeFileSync('test.ico', Buffer.from(JSON.parse(JSON.stringify(data)).data)))
-// console.log(Buffer.from(JSON.parse(JSON.stringify(data)).data).toString())
+const fs = require('fs');
+const initSqlJs = require('../sqljs-wasm/sql-wasm');
+const filebuffer = fs.readFileSync('out/fdbj/dictionary.db');
+initSqlJs().then(function (SQL) {
+    // Load the db
+    const db = new SQL.Database(filebuffer);
+    const diwr_1 = db.exec("SELECT name FROM sqlite_master WHERE type='table';")
+    const diwr_2 = db.exec("SELECT * FROM description")
+    console.log(diwr_2.values().next().value.values[0])
+
+    for(yg of diwr_1.values()){
+        // console.log(yg)
+    }
+}).catch(err => console.error(err))
