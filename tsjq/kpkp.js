8f818a73ccac824a4680af331da7e453d084512c
wrvr <974054997@qq.com>
2025-05-10 16:58:36 +0800

    sdvu kpkp

--- tsjq/kpkp.js
+++ tsjq/kpkp.js
@@ -7,6 +7,7 @@ const fo_ussk = require('../../scripts/fo_ussk')
 const ussk_cqpi = require('../../scripts/ussk_cqpi')
 const rfrf = require('../../scripts/rfrf')
 const getMyIp = require("../../scripts/getMyIp.js")
+const fdmj_wdbu = require('../../scripts/user_params-ldfs-atvn/fdmj_wdbu.js')
 const nikc_kpkp = path.resolve("out/kpkp")
 if (!fs.existsSync(nikc_kpkp)) {
     fs.mkdirSync(nikc_kpkp)
@@ -35,6 +36,26 @@ module.exports = [["kpkp"], {
     ## hd noph epni
     kpkp hd non
 
+    ## caum non zzuy 
+    kpkp zzuy non
+
+    ## caum tszn wu n zzuy
+    kpkp zzuy tszn
+    rqjt_wu_1
+    rqjt_wu_2
+    rqjt_wu_3
+
+    ## caum tszn wu n size zzuy
+    kpkp zzuy tszn --size
+    rqjt_wu_1
+    rqjt_wu_2
+    rqjt_wu_3
+
+    ## caum jc znzk wlba atvn cqpi stat eowl n zzuy
+    kpkp zzuy non --atvn
+    (diwr_stat)=>{
+        return diwr_stat.size
+    }
     `
     , aoao_ji_ssvl: false
     , func: async (user_params, outputs, neig_kp = {}) => {
@@ -63,8 +84,19 @@ module.exports = [["kpkp"], {
 
                     ).set_hqtz("fo")
                         .vdum(user_params)
-                })
-                .set('hd', () => {
+                }).set('zzuy', () => {
+                    const atvn_zzuy_cqpi = (vnwm) => {
+                        const wlba_cqpi_stat = ussk_cqpi(new Map()
+                            .set('size', () => {
+                                return (stat) => stat.size
+                            }).set('atvn', () => {
+                                const atvn = eval(`(${user_params._[2] === "non" ? user_params.lastParams : fdmj_wdbu(user_params)[1]})`)
+                                return atvn
+                            })).set_hqtz("fo").setDefault(() => JSON.stringify).vdum(user_params)
+                        return vnwm.map(rn1 => rn1 + ": " + wlba_cqpi_stat(fs.statSync(path.join(nikc_kpkp, rn1)))).join("\n")
+                    }
+                    return epni_cqpi(atvn_zzuy_cqpi, user_params)
+                }).set('hd', () => {
                     const atvn_hd_epni = (vnwm_epni) => {
                         const map_epni = new Map()
                         vnwm_epni.forEach(rn1 => {
@@ -80,15 +112,7 @@ module.exports = [["kpkp"], {
                     const atvn_cqpi_hd = (vnwm_epni) => {
                         return [...atvn_hd_epni(vnwm_epni)].map(rn1 => JSON.stringify(rn1)).join("\n")
                     }
-                    return ussk_cqpi(new Map()
-                        .set("non", () => {
-                            return atvn_cqpi_hd(fs.readdirSync(nikc_kpkp))
-                        })
-                        .set("tszn", () => {
-                            return atvn_cqpi_hd(bsVnwm(user_params))
-                        })
-                    )
-                        .vdum(user_params._[2])
+                    return epni_cqpi(atvn_cqpi_hd, user_params)
                 })
             )
                 .vdum(user_params._[1])
@@ -114,4 +138,12 @@ async function voud_ttfz_tsjq(user_params, vnwm_rjqt_wu, neig) {
             nikc_zzzz: "out/kpkp"
         }
     }).catch(e => console.error(e))
+}
+function epni_cqpi(atvn_wlba = (vnwm_nixb_rjqt_wu) => { }, user_params) {
+    return ussk_cqpi(new Map().set("non", () => {
+        return atvn_wlba(fs.readdirSync(nikc_kpkp))
+    }).set("tszn", () => {
+        return atvn_wlba(/\n-fdmj-/.test(user_params.lastParams) ? fdmj_wdbu(user_params)[2].split(/\n/).map(rn1 => rn1.trim()) : bsVnwm(user_params))
+    })).vdum(user_params._[2])
+
 }
